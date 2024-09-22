b2a799efb9ce4974ccf706144c87920e8b42704f
Raileanu Vlad <raileanu1100@gmail.com>
2024-09-22 22:02:34 +0300

    forgot to add the menu file

---
+++ menu.cpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+
+void mainMenu()
+{
+    std::cout << "Apasa 1 pentru a lucra cu coordonatele \n";
+    std::cout << "Apasa 2 pentru a lucra cu queue-ul \n";
+    std::cout << "Apasa 3 pentru a iesi \n";
+}
+
+void coordMenu()
+{
+    std::cout << "Apasa 1 pentru a initializa punctul 1 \n";
+    std::cout << "Apasa 2 pentru a initializa punctul 2 \n";
+    std::cout << "Apasa 3 pentru a adauga un numar la punctul 2 \n";
+    std::cout << "Apasa 4 pentru a adugaa impreuna aceste 2 puncte \n";
+    std::cout << "Apasa 5 pentru a scadea un numar de la punctul 1 \n";
+    std::cout << "Apasa 6 pentru a compara punctele \n";
+    std::cout << "Apasa 7 pentru a printa coordonatele \n";
+    std::cout << "Apasa 0 pentru a iesi \n";
+}
+void queueMenu()
+{ 
+    std::cout << "Apasa 1 pentru a umple queue-ul 1 \n";
+    std::cout << "Apasa 2 pentru a umple queue-ul 2 \n";
+    std::cout << "Apasa 3 pentru a le aduaga impreuna \n";
+    std::cout << "Apasa 4 pentru a verifica daca elee sunt egale \n";
+    std::cout << "Apasa 5 pentru a vedea care e mai mare \n";
+    std::cout << "Apasa 6 pentru a le printa la ecran \n";
+    std::cout << "Apasa 0 pentru a iesi \n";
+
+}
